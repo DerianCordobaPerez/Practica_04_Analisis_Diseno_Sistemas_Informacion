42d8b50a334fcebad37f1c76036eccee90f46b8c
DerianCordobaPerez <derianunanleon@gmail.com>
2022-02-15 20:56:26 -0600

    feat(Root): Add headers middleware and babel compilation

--- src/server.js
+++ src/server.js
@@ -6,10 +6,12 @@ import { xframe, xssProtection } from 'lusca'
 import compression from 'compression'
 import methodOverride from 'method-override'
 import expressFlash from 'express-flash'
+import session from 'express-session'
 import { engine } from 'express-handlebars'
 import { join } from 'path'
+import headers from './middlewares/headers'
 import homeRoutes from './routes/home.routes'
-import { PORT } from './config/constants'
+import { PORT, SESSION_SECRET } from './config/constants'
 
 // Initialize the server
 const app = Express()
@@ -30,8 +32,7 @@ app.set('view engine', '.hbs')
 
 // Middlewares
 app.use(Express.static(join(__dirname, 'public')))
-app.use(Express.json())
-app.use(Express.urlencoded({ extended: false }))
+app.use(headers)
 app.use(cors())
 app.use(helmet())
 app.use(compression())
@@ -39,6 +40,13 @@ app.use(methodOverride('_method'))
 app.use(expressFlash())
 app.use(xframe('SAMEORIGIN'))
 app.use(xssProtection(true))
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
 
 // Routes
 app.use(homeRoutes)
